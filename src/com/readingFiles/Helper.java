e9ed8155aea481f2bd603bc1abea4879bddd5790
Ngqanekana <mfundo.v.ngqanekana@accenture.com>
2020-05-22 15:20:08 +0200

    Creating a Helper Class of two methods to read and write form a file

---
+++ src/com/readingFiles/Helper.java
@@ -0,0 +1,17 @@
+package com.readingFiles;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Helper {
+
+public static Reader openReader(String fileName) throws IOException {
+    return Files.newBufferedReader(Paths.get(fileName));
+}
+public static Writer openWriter(String fileName) throws IOException{
+    return Files.newBufferedWriter(Paths.get(fileName));
+}
+}
